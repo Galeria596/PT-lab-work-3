6af1615bb95553e5d5a2313b198c9253573c3c61
Galeria596 <103591337+Galeria596@users.noreply.github.com>
2023-04-20 19:33:18 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace LabWork3
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInflation = new System.Windows.Forms.Button();
+            this.btnPopulation = new System.Windows.Forms.Button();
+            this.btnSalary = new System.Windows.Forms.Button();
+            this.btnLangRU = new System.Windows.Forms.Button();
+            this.btnLangEN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInflation
+            // 
+            this.btnInflation.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(95)))), ((int)(((byte)(184)))));
+            this.btnInflation.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnInflation.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(127)))), ((int)(((byte)(198)))));
+            this.btnInflation.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(111)))), ((int)(((byte)(192)))));
+            this.btnInflation.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInflation.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInflation.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnInflation.Location = new System.Drawing.Point(348, 112);
+            this.btnInflation.Margin = new System.Windows.Forms.Padding(12, 14, 12, 14);
+            this.btnInflation.Name = "btnInflation";
+            this.btnInflation.Size = new System.Drawing.Size(240, 64);
+            this.btnInflation.TabIndex = 12;
+            this.btnInflation.Text = "Inflation";
+            this.btnInflation.UseVisualStyleBackColor = false;
+            this.btnInflation.Click += new System.EventHandler(this.btnInflation_Click);
+            // 
+            // btnPopulation
+            // 
+            this.btnPopulation.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(95)))), ((int)(((byte)(184)))));
+            this.btnPopulation.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnPopulation.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(127)))), ((int)(((byte)(198)))));
+            this.btnPopulation.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(111)))), ((int)(((byte)(192)))));
+            this.btnPopulation.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPopulation.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPopulation.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnPopulation.Location = new System.Drawing.Point(348, 296);
+            this.btnPopulation.Margin = new System.Windows.Forms.Padding(12, 14, 12, 14);
+            this.btnPopulation.Name = "btnPopulation";
+            this.btnPopulation.Size = new System.Drawing.Size(240, 64);
+            this.btnPopulation.TabIndex = 13;
+            this.btnPopulation.Text = "Population";
+            this.btnPopulation.UseVisualStyleBackColor = false;
+            this.btnPopulation.Click += new System.EventHandler(this.btnPopulation_Click);
+            // 
+            // btnSalary
+            // 
+            this.btnSalary.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(95)))), ((int)(((byte)(184)))));
+            this.btnSalary.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnSalary.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(127)))), ((int)(((byte)(198)))));
+            this.btnSalary.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(111)))), ((int)(((byte)(192)))));
+            this.btnSalary.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSalary.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalary.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSalary.Location = new System.Drawing.Point(348, 204);
+            this.btnSalary.Margin = new System.Windows.Forms.Padding(12, 14, 12, 14);
+            this.btnSalary.Name = "btnSalary";
+            this.btnSalary.Size = new System.Drawing.Size(240, 64);
+            this.btnSalary.TabIndex = 14;
+            this.btnSalary.Text = "Median salary";
+            this.btnSalary.UseVisualStyleBackColor = false;
+            this.btnSalary.Click += new System.EventHandler(this.btnSalary_Click);
+            // 
+            // btnLangRU
+            // 
+            this.btnLangRU.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnLangRU.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnLangRU.FlatAppearance.BorderSize = 0;
+            this.btnLangRU.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkGray;
+            this.btnLangRU.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.btnLangRU.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLangRU.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnLangRU.ForeColor = System.Drawing.Color.Black;
+            this.btnLangRU.Location = new System.Drawing.Point(77, 518);
+            this.btnLangRU.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLangRU.Name = "btnLangRU";
+            this.btnLangRU.Size = new System.Drawing.Size(56, 30);
+            this.btnLangRU.TabIndex = 15;
+            this.btnLangRU.Text = "RU";
+            this.btnLangRU.UseVisualStyleBackColor = false;
+            this.btnLangRU.Click += new System.EventHandler(this.btnLangRU_Click);
+            // 
+            // btnLangEN
+            // 
+            this.btnLangEN.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnLangEN.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnLangEN.FlatAppearance.BorderSize = 0;
+            this.btnLangEN.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkGray;
+            this.btnLangEN.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Silver;
+            this.btnLangEN.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLangEN.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnLangEN.ForeColor = System.Drawing.Color.Black;
+            this.btnLangEN.Location = new System.Drawing.Point(13, 518);
+            this.btnLangEN.Margin = new System.Windows.Forms.Padding(4);
+            this.btnLangEN.Name = "btnLangEN";
+            this.btnLangEN.Size = new System.Drawing.Size(56, 30);
+            this.btnLangEN.TabIndex = 16;
+            this.btnLangEN.Text = "EN";
+            this.btnLangEN.UseVisualStyleBackColor = false;
+            this.btnLangEN.Click += new System.EventHandler(this.btnLangEN_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.btnLangEN);
+            this.Controls.Add(this.btnLangRU);
+            this.Controls.Add(this.btnSalary);
+            this.Controls.Add(this.btnPopulation);
+            this.Controls.Add(this.btnInflation);
+            this.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FormMain";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Lab-3          ";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnInflation;
+        private System.Windows.Forms.Button btnPopulation;
+        private System.Windows.Forms.Button btnSalary;
+        private System.Windows.Forms.Button btnLangRU;
+        private System.Windows.Forms.Button btnLangEN;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabWork3
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnInflation_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalary_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPopulation_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Смена языка на енгельский (Если идея - хуита, то можно потом делитнуть)
+        private void btnLangEN_Click(object sender, EventArgs e)
+        {
+            if (btnInflation.Text != "Inflation" || btnPopulation.Text != "Population" || btnSalary.Text != "Median salary")
+            {
+                btnInflation.Text = "Inflation";
+                btnPopulation.Text = "Population";
+                btnSalary.Text = "Median Salary";
+            }
+        }
+
+        // Смена языка на русский
+        private void btnLangRU_Click(object sender, EventArgs e)
+        {
+            if (btnInflation.Text == "Inflation" || btnPopulation.Text == "Population" || btnSalary.Text == "Median salary")
+            {
+                btnInflation.Text = "Инфляция";
+                btnPopulation.Text = "Население";
+                btnSalary.Text = "Медианная зарплата";
+            }
+        }
+    }
+}
