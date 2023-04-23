e4aa380bb01634d4191981dc948be665deaceeaf
Maria <poroshhina@yandex.ru>
2023-04-23 18:55:38 +0300

    Изменила вывод графика, теперь он выводится ко всем таблицам, кроме последней. По ней проводятся вычисления

--- MedianSalary.Designer.cs
+++ MedianSalary.Designer.cs
@@ -29,47 +29,60 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ВыборФайлаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
-            this.построитьГрафикToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(460, 45);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 108);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(299, 398);
+            this.dataGridView1.Size = new System.Drawing.Size(376, 522);
             this.dataGridView1.TabIndex = 4;
             // 
             // menuStrip1
             // 
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.файлToolStripMenuItem,
-            this.построитьГрафикToolStripMenuItem});
+            this.файлToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(808, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(1114, 28);
             this.menuStrip1.TabIndex = 3;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // файлToolStripMenuItem
             // 
             this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ВыборФайлаToolStripMenuItem,
             this.toolStripComboBox1});
             this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
-            this.файлToolStripMenuItem.Size = new System.Drawing.Size(117, 24);
-            this.файлToolStripMenuItem.Text = "Выбор файла";
-            this.файлToolStripMenuItem.Click += new System.EventHandler(this.файлToolStripMenuItem_Click);
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(79, 24);
+            this.файлToolStripMenuItem.Text = "Расчеты";
+            
+            // 
+            // ВыборФайлаToolStripMenuItem
+            // 
+            this.ВыборФайлаToolStripMenuItem.Name = "ВыборФайлаToolStripMenuItem";
+            this.ВыборФайлаToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ВыборФайлаToolStripMenuItem.Text = "Выбор файла";
+            this.ВыборФайлаToolStripMenuItem.Click += new System.EventHandler(this.ВыборФайлаToolStripMenuItem_Click);
             // 
             // toolStripComboBox1
             // 
@@ -77,13 +90,6 @@
             this.toolStripComboBox1.Size = new System.Drawing.Size(121, 28);
             this.toolStripComboBox1.Text = "Лист";
             this.toolStripComboBox1.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBox1_SelectedIndexChanged);
-            // 
-            // построитьГрафикToolStripMenuItem
-            // 
-            this.построитьГрафикToolStripMenuItem.Name = "построитьГрафикToolStripMenuItem";
-            this.построитьГрафикToolStripMenuItem.Size = new System.Drawing.Size(150, 24);
-            this.построитьГрафикToolStripMenuItem.Text = "Построить график";
-            this.построитьГрафикToolStripMenuItem.Click += new System.EventHandler(this.построитьГрафикToolStripMenuItem_Click);
             // 
             // openFileDialog1
             // 
@@ -96,11 +102,37 @@
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
             // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(427, 108);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(675, 406);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(427, 530);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(354, 100);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
             // MedianSalary
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(808, 483);
+            this.ClientSize = new System.Drawing.Size(1114, 680);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.menuStrip1);
             this.Name = "MedianSalary";
@@ -108,6 +140,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -119,8 +152,10 @@
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
         private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
-        private System.Windows.Forms.ToolStripMenuItem построитьГрафикToolStripMenuItem;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem ВыборФайлаToolStripMenuItem;
     }
 }
--- MedianSalary.cs
+++ MedianSalary.cs
@@ -17,32 +17,16 @@ namespace LabWork3
 {
     public partial class MedianSalary : Form
     {
+        //Chart chart1 = new Chart();
         private string filename = string.Empty;
         //здесь храним листы из экселевского файла 
         private DataTableCollection table = null;
-        string[] list = new string[50];
         public MedianSalary()
         {
             InitializeComponent();
+            chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+
         }
-        //private int ExportExcel()
-        //{
-        //    Excel.Application ObjWorkExcel = new Excel.Application();
-        //    Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(openFileDialog1.FileName);
-        //    Excel.Worksheet ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1]; //получить 1-й лист
-        //    var lastCell = ObjWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);//последнюю ячейку
-        //                                                                                        // размеры базы
-        //    int lastColumn = (int)lastCell.Column;
-        //    int lastRow = (int)lastCell.Row;
-        //    // Перенос в промежуточный массив класса Form1: string[,] list = new string[50, 5]; 
-        //    for (int j = 0; j < 5; j++) //по всем колонкам
-        //        for (int i = 0; i < lastRow; i++) // по всем строкам
-        //            list[i, j] = ObjWorkSheet.Cells[i + 1, j + 1].Text.ToString(); //считываем данные
-        //    ObjWorkBook.Close(false, Type.Missing, Type.Missing); //закрыть не сохраняя
-        //    ObjWorkExcel.Quit(); // выйти из Excel
-        //    GC.Collect(); // убрать за собой
-        //    return lastRow;
-        //}
 
         //метод, который отвечает за открытие и считывание таблицы
         //принимает путь
@@ -73,86 +57,11 @@ namespace LabWork3
                 toolStripComboBox1.Items.Add(t.TableName);
             }
             //по умолчанию выбран первый элемент
-            //toolStripComboBox1.SelectedIndex = 0;
-
+            toolStripComboBox1.SelectedIndex = 0;
         }
 
-        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            ////блок обработки исключений
-            //try
-            //{
-            //    //результат общения с пользователем
-            //    DialogResult res = openFileDialog1.ShowDialog();
-            //    if (res == DialogResult.OK)
-            //    {
-            //        //путь к открытому файлу
-            //        filename = openFileDialog1.FileName;
-            //        //тайтл формы - путь к файлу
-            //        Text = filename;
-            //        //вызов  метода открытия базы данных
-            //        excel(filename);
-            //    }
-            //    else
-            //    {
-            //        //ссылка на ошибку
-            //        throw new Exception("Файл не выбран");
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-            //    MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            //}
-        }
-
-        private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            //после изменение индекса списка
-            //отображение таблицы в поле формы
-            DataTable Table = table[Convert.ToString(toolStripComboBox1.SelectedItem)];
-            //присваиваем выбранную таблицу для отображения
-            dataGridView1.DataSource = Table;
-        }
-
-        private void построитьГрафикToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            Excel.Application ObjWorkExcel = new Excel.Application();
-            Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(openFileDialog1.FileName);
-            Excel.Worksheet ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1]; //получить 1-й лист
-            var lastCell = ObjWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);//последнюю ячейку
-                                                                                                // размеры базы
-            int lastColumn = (int)lastCell.Column;
-            int lastRow = (int)lastCell.Row;
-            // Перенос в промежуточный массив класса Form1: string[,] list = new string[50, 5]; 
-            //for (int j = 0; j < 5; j++) //по всем колонкам
-            //    for (int i = 0; i < lastRow; i++) // по всем строкам
-            //        list[i, j] = ObjWorkSheet.Cells[i + 1, j + 1].Text.ToString(); //считываем данные
-            Chart myChart = new Chart();
-            myChart.Parent = this;
-            myChart.ChartAreas.Add(new ChartArea("Data"));
-            Series mySeriesOfPoint = new Series("Line");
-            mySeriesOfPoint.ChartType = SeriesChartType.Line;
-            mySeriesOfPoint.ChartArea = "Data";
-            int k = 2008;
-            for (int i = 1; i < lastRow; i++)
-            { //по всем колонкам
-                
-                // по всем строкам
-                list[i] = ObjWorkSheet.Cells[i+1, 2].Text.ToString();
-                mySeriesOfPoint.Points.AddXY(k,list[i]);
-                k += 1;
-            }
-            
-            myChart.Series.Add(mySeriesOfPoint);
-            ObjWorkBook.Close(false, Type.Missing, Type.Missing); //закрыть не сохраняя
-            ObjWorkExcel.Quit(); // выйти из Excel
-            
-            GC.Collect(); // убрать за собой
-            
-
-        }
-
-        private void файлToolStripMenuItem_Click(object sender, EventArgs e)
+        //открытие файла
+        private void ВыборФайлаToolStripMenuItem_Click(object sender, EventArgs e)
         {
             //блок обработки исключений
             try
@@ -174,12 +83,47 @@ namespace LabWork3
                     throw new Exception("Файл не выбран");
                 }
             }
+            //вывод ошибки
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
-        
+        private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //после изменение индекса списка
+            //отображение таблицы в поле формы
+            DataTable Table = table[Convert.ToString(toolStripComboBox1.SelectedItem)];
+            //присваиваем выбранную таблицу для отображения
+            dataGridView1.DataSource = Table;
+            //если выбрана 4 таблица, то график не строится
+            //выводятся результаты вычислений
+            if (toolStripComboBox1.SelectedIndex == 3)
+            {
+                //подсчет 
+                richTextBox1.Text = "Вычисление процента роста зарплат\n";
+                var k = Convert.ToDouble(dataGridView1.Rows[0].Cells[1].Value);
+                var g = Convert.ToDouble(dataGridView1.Rows[8].Cells[1].Value);
+                var r = (g-k)/k*100;
+                richTextBox1.Text += "Мужчины: " + Convert.ToString(Math.Round(r,3)) + "\n";
+
+                k = Convert.ToInt32(dataGridView1.Rows[0].Cells[2].Value);
+                g = Convert.ToInt32(dataGridView1.Rows[8].Cells[2].Value);
+                r = (g - k) / k * 100;
+                richTextBox1.Text += "Женщины: " + Convert.ToString(Math.Round(r,3));
+            }
+            //если выбрана иная таблица, то по ней выводится график
+            else
+            {
+                chart1.Series[0].Points.Clear();
+                for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                {
+                    double x = Convert.ToDouble(dataGridView1.Rows[i].Cells[0].Value);
+                    double y = Convert.ToDouble(dataGridView1.Rows[i].Cells[1].Value);
+                    chart1.Series[0].Points.AddXY(x,y);
+                }
+            }
+        }
     }
 }
